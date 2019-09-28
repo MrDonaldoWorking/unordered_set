6447f014b8d80ec1d5c7e404069fd02d78ab4b53
MrDonaldoPublic <Mr.Donaldo.CF@gmail.com>
2019-09-28 17:56:32 +0300

    removed set

--- unordered_set.h
+++ unordered_set.h
@@ -3,7 +3,6 @@
 
 #include <vector>
 #include <assert.h>
-#include <set>
 #include <functional>
 
 template <typename T>
@@ -21,17 +20,14 @@ struct unordered_set {
         us_const_iterator &operator=(us_const_iterator const&) = default;
 
         us_const_iterator &operator++() {
-            ++l_pos;
-
-            if (l_pos == data[v_pos].size()) {
-                if (v_pos == *bs.rbegin()) {
-                    v_pos = data.size();
-                    l_pos = 0;
-                    return *this;
+            if (l_pos + 1 != data[v_pos].size()) {
+                ++l_pos;
+            } else {
+                ++v_pos;
+                while (v_pos < data.size() && data[v_pos].empty()) {
+                    ++v_pos;
                 }
-                v_pos = *bs.lower_bound(v_pos + 1);
                 l_pos = 0;
-                return *this;
             }
 
             return *this;
@@ -39,7 +35,7 @@ struct unordered_set {
 
         us_const_iterator const operator++(int) {
             us_const_iterator res(*this);
-            ++*this;
+            ++(*this);
             return res;
         }
 
@@ -61,28 +57,25 @@ struct unordered_set {
             return &(data[v_pos][l_pos]);
         }
     private:
-        std::vector<std::vector<U>> const data;
-        std::set<size_t> bs;
+        std::vector<std::vector<T>> const& data;
         size_t v_pos = 0, l_pos = 0;
 
-        us_const_iterator(size_t l_pos, size_t v_pos,
-                          std::vector<std::vector<U>> const& d,
-                          std::set<size_t> s) :
-                data(d), bs(std::move(s)), l_pos(l_pos), v_pos(v_pos) {}
+        us_const_iterator(size_t const& l_pos, size_t const& v_pos,
+                          std::vector<std::vector<T>> const& d) :
+                data(d), l_pos(l_pos), v_pos(v_pos) {}
 
         friend struct unordered_set;
     };
 
     using value_type = T;
-    using iterator = us_const_iterator<T>;
-    using const_iterator = us_const_iterator<T>;
+    using iterator = us_const_iterator<T const>;
+    using const_iterator = us_const_iterator<T const>;
 
     unordered_set() {
         table.resize(len);
     }
 
     unordered_set(unordered_set const& u) : cnt(u.cnt), len(u.len) {
-        bucket = u.bucket;
         table = u.table;
     }
 
@@ -104,7 +97,6 @@ struct unordered_set {
     }
 
     void clear() noexcept {
-        bucket.clear();
         for (auto& l : table) {
             l.clear();
         }
@@ -118,30 +110,24 @@ struct unordered_set {
 
         size_t pos = h(val) % len;
         ++cnt;
-        bucket.insert(pos);
-
         table[pos].push_back(val);
 
-        if (3 * bucket.size() > 2 * len) {
-            reserve(2 * len);
+        if (table[pos].size() * 2 > cnt) {
+            reserve(len * 2);
         }
-        return std::make_pair(iterator(table[pos].size() - 1, pos, table,
-                bucket), true);
+
+        return std::make_pair(iterator(table[pos].size() - 1, pos, table), true);
     }
 
     iterator erase(const_iterator it) {
         --cnt;
-        if (table[it.v_pos].size() == 1) {
-            bucket.erase(it.v_pos);
-        }
 
         auto res = table[it.v_pos].erase(table[it.v_pos].begin() + it.l_pos);
-        return iterator(res - table[it.v_pos].begin(), it.v_pos, table, bucket);
+        return iterator(res - table[it.v_pos].begin(), it.v_pos, table);
     }
 
     void swap(unordered_set &us) noexcept {
         std::swap(us.table, table);
-        std::swap(us.bucket, bucket);
         std::swap(us.cnt, cnt);
         std::swap(us.len, len);
     }
@@ -156,55 +142,55 @@ struct unordered_set {
         return 0;
     }
 
-    iterator find(size_t key) const noexcept {
-        size_t pos = h(key);
-        for (auto it = table[pos].begin(); it != table[pos].end(); ++it) {
-            if (*it == key) {
-                return iterator(it - table[pos].begin(), pos, table, bucket);
+    iterator find(T key) const noexcept {
+        size_t pos = h(key) % len;
+        for (size_t i = 0; i < table[pos].size(); ++i) {
+            if (table[pos][i] == key) {
+                return iterator(i, pos, table);
             }
         }
         return end();
     }
 
     iterator begin() {
-        return iterator(0, *bucket.begin(), table, bucket);
+        return iterator(0, get_next_bucket(-1), table);
     }
 
     const_iterator begin() const {
-        return iterator(0, *bucket.begin(),
-                table, bucket);
+        return iterator(0, get_next_bucket(-1), table);
     }
 
     iterator end() {
-        return iterator(0, table.size(), table, bucket);
+        return iterator(0, table.size(), table);
     }
 
     const_iterator end() const {
-        return iterator(0, table.size(), table, bucket);
-    }
-
-    size_t bucket_count() const {
-        return bucket.size();
+        return iterator(0, table.size(), table);
     }
 
     void reserve(size_t sz) {
         len = std::max(len, sz);
-        unordered_set tmp(*this);
-
-        clear();
-        table.resize(len);
-        for (iterator it = tmp.begin(); it != tmp.end(); ++it) {
+        std::vector<std::vector<T>> new_table(len);
+        for (iterator it = begin(); it != end(); ++it) {
             size_t pos = h(*it);
-            bucket.insert(pos);
-            table[pos].insert(table[pos].begin(), *it);
+            new_table[pos].push_back(*it);
         }
+        table.swap(new_table);
     }
 
 private:
     std::vector<std::vector<T>> table;
-    std::set<size_t> bucket;
     std::hash<T> h;
     size_t cnt = 0, len = 100;
+
+    size_t get_next_bucket(size_t pos) {
+        ++pos;
+        while (pos < table.size() && table[pos].empty()) {
+            ++pos;
+        }
+
+        return pos;
+    }
 };
 
 #endif //UNORDERED_SET_H
